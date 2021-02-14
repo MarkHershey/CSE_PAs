1b4d842a0bd5cbb5b9c7d40ee2901832edd3fa81
Mark Huang <hhzeos@gmail.com>
2021-02-14 09:37:39 +0800

    Task 1

--- PA1/shell.c
+++ PA1/shell.c
@@ -105,7 +105,6 @@ int shellSummond(char **args)
   return 1;
 }
 
-
 /**
  * Allows one to check daemon process
  * 
@@ -174,7 +173,6 @@ int shellExit(char **args)
 {
   return 0;
 }
-
 
 /*
   Builtin function implementations.
@@ -268,11 +266,17 @@ char *shellReadLine(void)
   // read one line from stdin using getline()
 
   // 1. Allocate a memory space to contain the string of input from stdin using malloc. Malloc should return a char* that persists even after this function terminates.
+  size_t size = SHELL_BUFFERSIZE;
+  char *buffer = (char *)malloc(size);
   // 2. Check that the char* returned by malloc is not NULL
-  // 3. Fetch an entire line from input stream stdin using getline() function. getline() will store user input onto the memory location allocated in (1)
+  if (buffer != NULL)
+  {
+    // 3. Fetch an entire line from input stream stdin using getline() function. getline() will store user input onto the memory location allocated in (1)
+    getline(&buffer, &size, stdin);
+  }
   // 4. Return the char*
 
-  return NULL;
+  return buffer;
 }
 
 /**
@@ -302,9 +306,8 @@ void shellLoop(void)
   char **args; // to tokenize them as arguments separated by spaces
   int status;  // to tell the shell program whether to terminate shell or not
 
-
   /** TASK 5 **/
-  //write a loop where you do the following: 
+  //write a loop where you do the following:
 
   // 1. print the message prompt
   // 2. clear the buffer and move the output to the console using fflush
@@ -314,9 +317,7 @@ void shellLoop(void)
 
   // 6. free memory location containing the strings of characters
   // 7. free memory location containing char* to the first letter of each word in the input string
-  // 8. check if shellExecuteInput returns 1. If yes, loop back to Step 1 and prompt user with new input. Otherwise, exit the shell. 
-
-
+  // 8. check if shellExecuteInput returns 1. If yes, loop back to Step 1 and prompt user with new input. Otherwise, exit the shell.
 }
 
 int main(int argc, char **argv)
@@ -325,7 +326,11 @@ int main(int argc, char **argv)
   printf("Shell Run successful. Running now: \n");
 
   // Run command loop
-  shellLoop();
+  // shellLoop();
+
+  // check task 1
+  char *line = shellReadLine();
+  printf("The fetched line is : %s \n", line);
 
   return 0;
 }
