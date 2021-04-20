df72d47ebdd33955a2c4a2faee65dfff5cdbb86c
Mark Huang <hhzeos@gmail.com>
2021-04-20 12:02:47 +0800

    Clean up

--- ProgrammingAssignment2/PA2/regress_plot.py
+++ ProgrammingAssignment2/PA2/regress_plot.py
@@ -3,18 +3,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
-data = pd.read_csv("time_ClientCP2.csv", sep = "\t")
+data = pd.read_csv("stats/time_ClientCP2.csv", sep="\t")
 size = data.iloc[:, 0].values.reshape(-1, 1)
 time = data.iloc[:, 1].values.reshape(-1, 1)
 linear_regressor = LinearRegression()
 linear_regressor.fit(size, time)
 Y_pred = linear_regressor.predict(size)  # make predictions
-print(linear_regressor.coef_,
-      linear_regressor.intercept_,
-      linear_regressor.score(size, time))
+print(
+    linear_regressor.coef_,
+    linear_regressor.intercept_,
+    linear_regressor.score(size, time),
+)
 
 plt.scatter(size, time)
-plt.plot(size, Y_pred, color='red')
+plt.plot(size, Y_pred, color="red")
 plt.xlabel("Filesize (bits)")
 plt.ylabel("Time taken (s)")
 plt.show()
--- ProgrammingAssignment2/PA2/time_client.py
+++ ProgrammingAssignment2/PA2/time_client.py
@@ -9,8 +9,8 @@ times = 20
 client = "ClientCP1"
 server = "ServerCP1"
 
-with open("time_"+client+".csv", "w") as outfile:
-    for size in range(step, step*(times+1), step):
+with open("stats/time_" + client + ".csv", "w") as outfile:
+    for size in range(step, step * (times + 1), step):
         print(size)
         with open("client_res/test.bin", "wb") as f:
             f.write(random.randbytes(size))
